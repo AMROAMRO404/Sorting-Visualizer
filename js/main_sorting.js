0536611cdc4b9c2dd89c3498bac05407c8677671
AMROAMRO404 <amro.amro1999@gmail.com>
2021-07-11 17:52:16 +0300

    edit style

--- js/main_sorting.js
+++ js/main_sorting.js
@@ -1,42 +1,45 @@
 let array = [];
 let NumberOfBars = 50;
 function createNewArray() {
-    document.querySelector("#bars").innerHTML = ''; // to delete the past bars
-    array = [];
-    for (let i = 0; i < NumberOfBars; i++) {
-        array.push(Math.floor(Math.random() * 250) + 5);
-    }
-    const bars = document.querySelector("#bars");
-    for (let i = 0; i < NumberOfBars; i++) {
-        const bar = document.createElement("div");
-        bar.style.height = `${array[i]*2}px`;
-        bar.classList.add('bar');
-        bar.classList.add('flex-item');
-        bar.classList.add(`bar_No${i}`);
-        bars.appendChild(bar);
-    }
+  document.querySelector("#bars").innerHTML = ""; // to delete the past bars
+  array = [];
+  for (let i = 0; i < NumberOfBars; i++) {
+    array.push(Math.floor(Math.random() * 270) + 5);
+  }
+  const bars = document.querySelector("#bars");
+  for (let i = 0; i < NumberOfBars; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = `${array[i] * 2}px`;
+    bar.classList.add("bar");
+    bar.classList.add("vertical_line");
+    bar.classList.add(`bar_No${i}`);
+    bars.appendChild(bar);
+  }
+  // html example
+  //   <span>&#124;</span>;
 }
 
 let delay = 50;
-function setDelay(milisec) { 
-    return new Promise(resolve => { 
-        setTimeout(() => { resolve('') }, milisec); 
-    }) 
+function setDelay(milisec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("");
+    }, milisec);
+  });
 }
 
 const speedBtn = document.querySelector("#speed_sorting");
-speedBtn.addEventListener('input', async function(){
-    delay = parseInt(speedBtn.value);
+speedBtn.addEventListener("input", async function () {
+  delay = parseInt(speedBtn.value);
 });
 
-
 const sizeBtn = document.querySelector("#array_size");
-sizeBtn.addEventListener('input', async function(){
-    NumberOfBars = parseInt(sizeBtn.value);
-    createNewArray();
+sizeBtn.addEventListener("input", async function () {
+  NumberOfBars = parseInt(sizeBtn.value);
+  createNewArray();
 });
 
 const new_arrayBtn = document.querySelector("#new_array");
-new_arrayBtn.addEventListener('click', async function(){
-    createNewArray();
+new_arrayBtn.addEventListener("click", async function () {
+  createNewArray();
 });
